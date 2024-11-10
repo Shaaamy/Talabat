1b5d1eb1dd145b1f94ad0476721ae5d54358ac95
Amr Shamy <104179365+Shaaamy@users.noreply.github.com>
2024-11-10 10:26:44 +0200

    update product
    sss

--- Talabat.Core/Entities/Product.cs
+++ Talabat.Core/Entities/Product.cs
@@ -8,7 +8,6 @@ namespace Talabat.Core.Entities
 {
     public class Product : BaseEntity
     {
-        public int id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string PictureUrl { get; set; }

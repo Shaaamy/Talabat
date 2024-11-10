069ed6be5fbe3b9f90b590818c8c568ebe2c6e6f
Amr Shamy <104179365+Shaaamy@users.noreply.github.com>
2024-11-10 10:26:04 +0200

    update product
    add id

--- Talabat.Core/Entities/Product.cs
+++ Talabat.Core/Entities/Product.cs
@@ -8,6 +8,7 @@ namespace Talabat.Core.Entities
 {
     public class Product : BaseEntity
     {
+        public int id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string PictureUrl { get; set; }
